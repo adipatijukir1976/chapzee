99b1febba12b5ba6d436d693be91b5d354fdaecb
adipatijukir1976 <bojonegoro.developer@gmail.com>
2025-07-05 16:27:41 +0700

    Update hot.py

--- api/hot.py
+++ api/hot.py
@@ -3,9 +3,9 @@ from scrape import scrape_hot_bge_with_page
 
 hot_bp = Blueprint("hot", __name__)
 
-@hot_bp.route("/hot")
-def hot():
-    tipe = request.args.get("tipe", "manga")
+@hot_bp.route("/hot/<tipe>")
+def hot(tipe):
     page = int(request.args.get("page", 1))
     data = scrape_hot_bge_with_page(tipe=tipe, page=page)
     return jsonify(data)
+
