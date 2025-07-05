9f0f4f56b33d6718d2b5e0f9a6b8e9a01c27699f
adipatijukir1976 <bojonegoro.developer@gmail.com>
2025-07-05 16:25:46 +0700

    Update update.py

--- api/update.py
+++ api/update.py
@@ -3,9 +3,9 @@ from scrape import scrape_paginated_bge_with_page
 
 update_bp = Blueprint("update", __name__)
 
-@update_bp.route("/update")
-def update():
-    tipe = request.args.get("tipe", "manga")
+@update_bp.route("/update/<tipe>")
+def update(tipe):
     page = int(request.args.get("page", 1))
     data = scrape_paginated_bge_with_page(tipe=tipe, page=page)
     return jsonify(data)
+
