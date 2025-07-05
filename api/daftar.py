da27ca6cc859a40e64a534590a307664dccf4088
adipatijukir1976 <bojonegoro.developer@gmail.com>
2025-07-05 16:29:09 +0700

    Update daftar.py

--- api/daftar.py
+++ api/daftar.py
@@ -3,9 +3,9 @@ from scrape import scrape_paginated_bge_with_page
 
 daftar_bp = Blueprint("daftar", __name__)
 
-@daftar_bp.route("/daftar")
-def daftar():
-    tipe = request.args.get("tipe", "manga")
+@daftar_bp.route("/daftar/<tipe>")
+def daftar(tipe):
     page = int(request.args.get("page", 1))
     data = scrape_paginated_bge_with_page(tipe=tipe, page=page)
     return jsonify(data)
+
